887af0870013230439b5bc0bd5f3d354873a4952
Rohit Bulusu <rohitbulusu@gmail.com>
2020-05-20 20:57:42 -0400

    added base code for pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:worldtimeapp/pages/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
 }
